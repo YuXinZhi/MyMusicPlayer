6b962373de146fcf7a8fe640efedaa959314b1ef
Ben <Ben@Ben-PC.mshome.net>
2016-04-13 21:54:17 +0800

    Created MusicBrowserActivity

--- src/com/example/mymusicplayer/ui/MusicBrowserActivity.java
+++ src/com/example/mymusicplayer/ui/MusicBrowserActivity.java
@@ -1,7 +1,30 @@
 package com.example.mymusicplayer.ui;
 
+import com.example.mymusicplayer.R;
+import com.example.mymusicplayer.utils.MusicUtils;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MusicBrowserActivity extends Activity {
+
+	public MusicBrowserActivity() {
+	}
+
+	/**
+	 * activity第一次创建时调用
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		int activeTab=MusicUtils.getIntPref(this,"activeTab",R.id.artisttab);
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 
 }
--- src/com/example/mymusicplayer/utils/MusicUtils.java
+++ src/com/example/mymusicplayer/utils/MusicUtils.java
@@ -1,8 +1,23 @@
 package com.example.mymusicplayer.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class MusicUtils {
 
 	private static final String TAG = "MusicUtils";
-	
+
+	/**
+	 * 获取int类型的SharePreferences的值
+	 * 
+	 * @param context
+	 * @param name
+	 * @param def
+	 * @return
+	 */
+	 public static int getIntPref(Context context, String name, int def) {
+		SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+		return prefs.getInt(name, def);
+	}
 
 }
