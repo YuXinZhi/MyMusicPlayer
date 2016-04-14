6e9a6ef36083292e876ed74ead073f67df777206
Ben <Ben@Ben-PC>
2016-04-14 21:31:09 +0800

    added two layout

--- src/com/example/mymusicplayer/ui/MediaPlaybackActivity.java
+++ src/com/example/mymusicplayer/ui/MediaPlaybackActivity.java
@@ -1,10 +1,106 @@
 package com.example.mymusicplayer.ui;
 
+import com.example.mymusicplayer.utils.MusicUtils;
 import android.app.Activity;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MediaPlaybackActivity extends Activity {
+public class MediaPlaybackActivity extends Activity
+		implements MusicUtils.Defs, View.OnTouchListener, View.OnLongClickListener {
 
 	public MediaPlaybackActivity() {
 	}
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/**
+		 * 设置音量键控制的音量（音乐）
+		 */
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		
+	}
+
+	private static class Worker implements Runnable {
+
+		private final Object mLock = new Object();
+		private Looper mLooper;
+
+		/**
+		 * 创建一个name线程
+		 * 
+		 * @param name
+		 */
+		public Worker(String name) {
+			Thread t = new Thread(null, this, name);
+			t.setPriority(Thread.MIN_PRIORITY);
+			t.start();
+			synchronized (mLock) {
+				while (mLooper == null) {
+					try {
+						mLock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		public Looper getLooper() {
+			return mLooper;
+		}
+
+		@Override
+		public void run() {
+			synchronized (mLock) {
+				Looper.prepare();
+				mLooper = Looper.myLooper();
+				mLock.notifyAll();
+			}
+			Looper.loop();
+		}
+
+		public void quit() {
+			mLooper.quit();
+		}
+
+	}
+
+	/**
+	 * 轮询器 android.os.Looper
+	 * 
+	 * Class used to run a message loop for a thread. Threads by default do not
+	 * have a message loop associated with them; to create one, call prepare in
+	 * the thread that is to run the loop, and then loop to have it process
+	 * messages until the loop is stopped.
+	 * 
+	 * Most interaction with a message loop is through the Handler class.
+	 * 
+	 * This is a typical example of the implementation of a Looper thread, using
+	 * the separation of prepare and loop to create an initial Handler to
+	 * communicate with the Looper.
+	 * 
+	 * class LooperThread extends Thread { public Handler mHandler;
+	 * 
+	 * public void run() { Looper.prepare();
+	 * 
+	 * mHandler = new Handler() { public void handleMessage(Message msg) { //
+	 * process incoming messages here } };
+	 * 
+	 * Looper.loop(); } }
+	 * 
+	 */
+
+	@Override
+	public boolean onLongClick(View v) {
+		return false;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return false;
+	}
 }
--- src/com/example/mymusicplayer/utils/MusicUtils.java
+++ src/com/example/mymusicplayer/utils/MusicUtils.java
@@ -14,6 +14,25 @@ public class MusicUtils {
 
 	private static final String TAG = "MusicUtils";
 
+	public interface Defs {
+		public final static int OPEN_URL = 0;
+		public final static int ADD_TO_PLAYLIST = 1;
+		public final static int USE_AS_RINGTONE = 2;
+		public final static int PLAYLIST_SELECTED = 3;
+		public final static int NEW_PLAYLIST = 4;
+		public final static int PLAY_SELECTION = 5;
+		public final static int GOTO_START = 6;
+		public final static int GOTO_PLAYBACK = 7;
+		public final static int PARTY_SHUFFLE = 8;
+		public final static int SHUFFLE_ALL = 9;
+		public final static int DELETE_ITEM = 10;
+		public final static int SCAN_DONE = 11;
+		public final static int QUEUE = 12;
+		public final static int EFFECTS_PANEL = 13;
+		public final static int CHILD_MENU_BASE = 14; // this should be the last
+														// item
+	}
+
 	/**
 	 * 获取int类型的SharePreferences的值
 	 * 
@@ -61,12 +80,11 @@ public class MusicUtils {
 		intent.putExtra("withtabs", true);
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		/**
-		 * Intent.FLAG_ACTIVITY_CLEAR_TOP
-		 * If set, and the activity being launched is already running in the
-		 * current task, then instead of launching a new instance of that
-		 * activity, all of the other activities on top of it will be closed and
-		 * this Intent will be delivered to the (now on top) old activity as a
-		 * new Intent.
+		 * Intent.FLAG_ACTIVITY_CLEAR_TOP If set, and the activity being
+		 * launched is already running in the current task, then instead of
+		 * launching a new instance of that activity, all of the other
+		 * activities on top of it will be closed and this Intent will be
+		 * delivered to the (now on top) old activity as a new Intent.
 		 */
 		activity.startActivity(intent);
 		activity.finish();
