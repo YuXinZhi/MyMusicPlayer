21a0fdc3294fae5e7c47a79f527a99b97072277c
Ben <Ben@Ben-PC>
2016-04-27 23:06:17 +0800

    4272306

--- src/com/example/mymusicplayer/ui/MusicBrowserActivity.java
+++ src/com/example/mymusicplayer/ui/MusicBrowserActivity.java
@@ -1,12 +1,21 @@
 package com.example.mymusicplayer.ui;
 
+import com.example.mymusicplayer.IMediaPlaybackService;
 import com.example.mymusicplayer.R;
+import com.example.mymusicplayer.service.MediaPlaybackService;
 import com.example.mymusicplayer.utils.MusicUtils;
+import com.example.mymusicplayer.utils.MusicUtils.ServiceToken;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 
-public class MusicBrowserActivity extends Activity {
+//MusicUtils.Defs是内部接口
+public class MusicBrowserActivity extends Activity implements MusicUtils.Defs {
+	ServiceToken mToken;
 
 	public MusicBrowserActivity() {
 	}
@@ -28,11 +37,46 @@ public class MusicBrowserActivity extends Activity {
 			activeTab = R.id.artisttab;
 		}
 		MusicUtils.activateTab(this, activeTab);
+
+		String shuf = getIntent().getStringExtra("sutoshuffle");
+		if ("true".equals(shuf)) {
+			mToken = MusicUtils.bindToService(this, autoshuffle);
+		}
 	}
 
 	@Override
 	protected void onDestroy() {
+		if (mToken!=null) {
+			MusicUtils.unbindFromService(mToken);
+		}
 		super.onDestroy();
 	}
+
+	private ServiceConnection autoshuffle = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// we need to be able to bind again, so unbind
+			// 需要能够再次绑定，所以线解绑
+			try {
+				unbindService(this);
+			} catch (IllegalArgumentException e) {
+			}
+
+			IMediaPlaybackService serv = IMediaPlaybackService.Stub.asInterface(service);
+			if (serv != null) {
+				try {
+					serv.setShuffleMode(MediaPlaybackService.SHUFFLE_AUTO);
+				} catch (RemoteException ex) {
+				}
+			}
+
+		}
+	};
 
 }
